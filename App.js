24bc1c3d06000706a4643e6a819f1350e895c11b
David Duncker <dave.duncker@gmail.com>
2022-10-23 23:56:48 -0400

    Experiments with HTTP requests

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react'
-import { printSomething } from './helper_functions';
+import { printSomething, getHTTPresponse, printNestedObject } from './helper_functions';
 
 
 export default class App extends React.Component {
@@ -9,8 +9,48 @@ export default class App extends React.Component {
   state = { text: "Open up App.js to start working on your app!"}
 
   componentDidMount() {
-    printSomething()
-  }
+    //printSomething()
+    let url1 = "http://www.google.com/";
+    let response1 = getHTTPresponse(url1);
+    console.log("First HTTP response: ");
+    printNestedObject(response1);
+
+    let url2 = "https://jsonplaceholder.typicode.com/posts/1/";
+    fetch(url2, 
+      { method: "GET" })
+      .then( (response2) => {
+        console.log("Second HTTP response: ");
+        printNestedObject(response2);
+        console.log("Extracting JSON");
+        return response2.json();
+      })
+      .then( (responseJSON) => {
+        console.log("HTTP JSON response:");
+        printNestedObject(responseJSON);
+      })
+
+    let url3 = "https://jsonplaceholder.typicode.com/posts/1/";
+    console.log("Third HTTP request")
+    fetch(url3, 
+      { method: "GET" })
+      .then( (response3) => {
+        console.log("Third HTTP response: ");
+        printNestedObject(response3);
+        console.log("Extracting body");
+        return response3 .toString();
+      })
+      .then( (response3_2) => {
+        console.log("HTTP response:");
+        console.log(response3_2);
+      })
+  
+    let url4 = "https://www.google.com";
+    const request = new XMLHttpRequest();
+    console.log("Fourth HTTP request")
+    request.addEventListener("load", () => { console.log(this.responseText)});
+    request.open("GET", url4);
+    request.send();
+}
 
   render() {
     return (
--- helper_functions.js
+++ helper_functions.js
@@ -1,3 +1,25 @@
+export function printNestedObject(obj, depth=0) {
+    indent = "  ".repeat(depth)
+    for (let [key, value] of Object.entries(obj)) {
+        if ((typeof(value) != "object") || (value == null)) {
+            let output = indent + String(key) + ": " + String(value)
+            console.log(output)
+        }
+        else {
+            console.log(indent + String(key) + ": ")
+            printNestedObject(value, depth+1)
+        }
+    }
+}
+
+export async function getHTTPresponse(url) {
+    let http_response = ""
+    fetch(url, 
+        { method: "GET" })
+        .then( (response) => http_response = response)
+    return http_response
+}
+
 export function printSomething() {
     console.log("Something")
 }
